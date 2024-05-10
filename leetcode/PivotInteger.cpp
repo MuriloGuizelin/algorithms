80f1bb72506a268233bc9135057fecda0cb16510
Murilo Guizelin <66173617+MuriloGuizelin@users.noreply.github.com>
2024-05-10 19:27:46 -0400

    Update PivotInteger.cpp

--- leetcode/PivotInteger.cpp
+++ leetcode/PivotInteger.cpp
@@ -14,5 +14,5 @@ public:
 };
 
 // Calcula se é um quadrado perfeito
-// COMO? -> Se a raiz quadrada de um número for um número inteiro, então é um quadrado perfeito e é o pivo.
+// Se a raiz quadrada de um número for um número inteiro, então é um quadrado perfeito e é o pivo.
 // Exercicio resolvido com matematica pura e logica aplicada
