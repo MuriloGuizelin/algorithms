f68a2b2fff16ce658f5c28dd47e4bc7217769033
Murilo Guizelin <murilomotta4@gmail.com>
2024-05-12 17:10:10 -0400

    obi 2021 torneio exercise

---
+++ Exercicios/torneio.java
@@ -0,0 +1,27 @@
+package Exercicios;
+import java.util.Scanner;
+
+public class torneio{
+    public static void main(String[] args){
+        int v = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<6;i++){
+            String s = sc.nextLine();
+            if(s.equals("V")){
+                v = v + 1;
+            }
+        }
+        if(v > 4){
+            System.out.println("1");
+        }
+        else if(v > 2){
+            System.out.println("2");
+        }
+        else if(v > 0){
+            System.out.println("3");
+        }
+        else{
+            System.out.println("-1");
+        }
+    }
+}
